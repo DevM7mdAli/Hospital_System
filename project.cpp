ca005330dc0904e8b79ad9eaaad977bf23da528b
Mohammed Ali <74423891+DevM7mdAli@users.noreply.github.com>
2023-01-27 11:44:26 +0300

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -37,17 +37,3 @@ int Size;
 
 
 }
-
-
-/*
-o adding new item.
-o search for specific item.
-o update existing record.
-o delete.
-o Sort.
-o Display.
-o At the start, your program should populate the array(s) from a file and allow the user to edit it. At
-the end overwrite the same file with the latest contents of the array(s).
-o Provide another statistical report as separate text file e.g. the number of records, with date, time
-of last update.
-*/
