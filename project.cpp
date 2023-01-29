be35982c2afc611d32afe7a8984935a075a9d297
Mustavoh <123810684+Mustavoh@users.noreply.github.com>
2023-01-29 10:35:46 +0300

    Menu Update, Fixed typos.

--- project.cpp
+++ project.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include <cmath>
 #include <string>
+#include <fstream>
 
 
 using namespace std;
 
-//prototype the functions 
+//prototype the functions
 void add();
-void serch();
+void search();
 void update();
 void del();
 void sort();
@@ -15,34 +15,34 @@ void print();
 
 //omar_Alsubgh;
 
-
 //Struct for patients
 struct patients
 {
-string name;
-int ID;
-int age;
-int PhoneNumber;
-string medicine;
-string dose;
+    string name;
+    int ID;
+    int age;
+    int PhoneNumber;
+    string medicine;
+    string dose;
 };
-
-
 
 int main()
 {
-int choose;
+    int choice;
 
-cout << "------------------------------------------------------------------------------------" << endl;
-cout << "\t\t\t welcome to the hospetal system" << endl;
-cout << "------------------------------------------------------------------------------------" << endl << endl;
+    cout << "|----------------------------------------------------------------------------------|" << endl;
+    cout << "\t\t\t HOSPITAL MANAGEMENT SYSTEM" << endl;
+    cout << "|----------------------------------------------------------------------------------|" << endl << endl;
 
-cout << "choose one of the options: " << endl
-     << "1- adding new item. " << endl
-     << "2- Search " << endl
-     << "3- update existing record " << endl
-     << "4- delete patient " << endl
-     << "5- Sort patients" << endl 
-     << "6- print all patients ";
-      cin >> choose;
+    cout << "Choose one of the options below: " << endl
+         << "[1] Add a patient " << endl
+         << "[2] Search for a patient" << endl
+         << "[3] Update patient information " << endl
+         << "[4] Delete patient " << endl
+         << "[5] Sort patients" << endl
+         << "[6] Display all patients "<<endl
+         << "[7] Exit Program "<<endl
+         << "Please enter your choice: ";
+    cin >> choice;
+//I updated the menu ~Mustafa
 }
